663dcd24a7f3c3af0ea64ed6cc83b3643ddb6199
horitaka <takahiro.ohori@gmail.com>
2020-05-31 18:08:00 +0900

    update: APIキーを環境変数に変更

--- config/index.js
+++ config/index.js
@@ -1,11 +1,12 @@
+require('dotenv').config()
+
 const config = {
   calil: {
     apiUrl: 'http://api.calil.jp',
-    apiKey: '1e337d58ad44c968c93dc772f684dc0a',
+    apiKey: process.env.CALIL_API_KEY,
   },
   googleBooks: {
     apiUrl: 'https://www.googleapis.com/books',
-
   }
 }
 
